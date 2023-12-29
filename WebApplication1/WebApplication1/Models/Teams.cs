4510f4d0ec28b7acfd76d10446c366b3a2f9c966
Naveenn789 <naveengalla77@gmail.com>
2023-12-29 17:33:41 +0530

    Web API Mvc

---
+++ WebApplication1/WebApplication1/Models/Teams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [Table("Teams")]
+    public class Teams
+    {
+        [Key]
+        public int TId { get; set; }
+        public string TName { get; set; }
+        public string TEmail { get; set; }
+        public string TSlogan { get; set; }
+
+    }
+}
